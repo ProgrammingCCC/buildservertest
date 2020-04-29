aff76fdcf83076f88530f72343e5993003c075fa
karl.statz@gmail.com <karl.statz@gmail.com>
2020-04-29 18:46:18 -0500

    adding files

---
+++ build/CMakeFiles/buildtest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/buildtest.dir/src/main.cpp.o"
+  "buildtest.pdb"
+  "buildtest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/buildtest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
